bd400391dc0763002ea9dc37f74648177a551d0a
Evan Hubinger <evanjhub@gmail.com>
2019-01-11 11:52:12 -0800

    Add deltas, momentum

--- ProxyUtility.py
+++ ProxyUtility.py
@@ -10,6 +10,7 @@ import random
 import numpy as np
 import tensorflow as tf
 from itertools import count
+from matplotlib import pyplot as plt
 
 if sys.version_info < (3,):
     range = xrange
@@ -17,18 +18,20 @@ if sys.version_info < (3,):
 
 VERSION = 0.1
 
-NUM_RUNS = 10
+NUM_RUNS = 1
 
 ACTIVATION = "tanh"
 
-INPUT_SIZE         = 10
-ARCHITECTURE_REAL  = [INPUT_SIZE] + [10, 10, 10]
+INPUT_SIZE = 10
+ARCHITECTURE_REAL = [INPUT_SIZE] + [10, 10, 10]
 ARCHITECTURE_PROXY = [INPUT_SIZE] + [10, 10, 10]
-TRAINING_SAMPLES   = 200
-TRAINING_STEPS     = 2000
-LEARNING_RATE      = 0.1
-UNIFORM_X_VALS     = True
-UNIFORM_Y_VALS     = True
+REAL_TRAINING_STEPS = 2000
+PROXY_TRAINING_STEPS = 2000
+PROXY_TRAINING_SAMPLES = 200
+LEARNING_RATE = 0.1
+MOMENTUM = 0
+UNIFORM_X_VALS = True
+UNIFORM_Y_VALS = True
 
 NUM_SAMPLES = 1000000
 
@@ -48,6 +51,7 @@ class Net:
         self.output = tf.layers.dense(flow, 1, activation="tanh")
 
 def do_optimization(sess, title, loss, opt, training_steps, feed_dict):
+    sess.run(tf.global_variables_initializer())
     loss_plot = []
     for _ in range(training_steps):
         l, _ = sess.run(
@@ -66,7 +70,9 @@ def uniform(*args, **kwargs):
     return 2 * np.random.rand(*args, **kwargs) - 1
 
 
-for _ in range(NUM_RUNS):
+for run_i in range(NUM_RUNS):
+    print("Starting run {}/{}...".format(run_i + 1, NUM_RUNS))
+
     # Create all required networks.
     real_utility  = Net(ARCHITECTURE_REAL)
     proxy_utility = Net(ARCHITECTURE_PROXY)
@@ -86,23 +92,27 @@ for _ in range(NUM_RUNS):
     else:
         random_ys = np.tanh(np.random.randn(random_count, 1))
 
+    if MOMENTUM:
+        optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=MOMENTUM)
+    else:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+
 
     # Do a first pass of training real_utility on our random data.
     desired_output = tf.placeholder(tf.float32, [None, 1], name="desired_output")
     loss = tf.reduce_mean(
         tf.squared_difference(real_utility.output, desired_output),
     )
-    opt = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
+    opt = optimizer.minimize(loss)
 
     sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
 
     do_optimization(
         sess,
         "RealUtilityTraining",
         loss,
         opt,
-        TRAINING_STEPS,
+        REAL_TRAINING_STEPS,
         {
             real_utility.input: random_xs,
             desired_output: random_ys,
@@ -119,7 +129,7 @@ for _ in range(NUM_RUNS):
 
 
     # Now train proxy_utility to match real_utility.
-    training_set = genx(TRAINING_SAMPLES, INPUT_SIZE)
+    training_set = genx(PROXY_TRAINING_SAMPLES, INPUT_SIZE)
 
     loss = tf.reduce_mean(
         tf.squared_difference(
@@ -127,14 +137,14 @@ for _ in range(NUM_RUNS):
             proxy_utility.output,
         ),
     )
-    opt = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
+    opt = optimizer.minimize(loss)
 
     do_optimization(
         sess,
         "ProxyUtilityTraining",
         loss,
         opt,
-        TRAINING_STEPS,
+        PROXY_TRAINING_STEPS,
         {
             real_utility.input: training_set,
             proxy_utility.input: training_set,
@@ -192,17 +202,17 @@ for _ in range(NUM_RUNS):
 
     # Compute a scatter plot of proxy utility vs real utility on purely random inputs.
 
-    #SCATTER_COUNT = 100000
+    # SCATTER_COUNT = 100000
 
     samples = genx(NUM_SAMPLES, INPUT_SIZE)
     random_proxy_vals = compute_utility(proxy_utility, samples)
     random_real_vals = compute_utility(real_utility, samples)
-    #plt.scatter(random_proxy_vals[:SCATTER_COUNT], random_real_vals[:SCATTER_COUNT], alpha=0.01)
+    # plt.scatter(random_proxy_vals[:SCATTER_COUNT], random_real_vals[:SCATTER_COUNT], alpha=0.01)
 
     optimized_samples = optimize_samples(samples, OPT_STEP_SIZE, OPT_STEPS)
     optimized_proxy_vals = compute_utility(proxy_utility, optimized_samples)
     optimized_real_vals = compute_utility(real_utility, optimized_samples)
-    #plt.scatter(optimized_proxy_vals[:SCATTER_COUNT], optimized_real_vals[:SCATTER_COUNT], alpha=0.005)
+    # plt.scatter(optimized_proxy_vals[:SCATTER_COUNT], optimized_real_vals[:SCATTER_COUNT], alpha=0.005)
 
 
     # Write a file with the data.
@@ -227,6 +237,8 @@ for _ in range(NUM_RUNS):
     with open(output_name, "wb") as f:
         pickle.dump(
             {
+                # "samples": samples,
+                # "optimized_samples": optimized_samples,
                 "random_proxy_vals": random_proxy_vals,
                 "random_real_vals": random_real_vals,
                 "optimized_proxy_vals": optimized_proxy_vals,
--- plot.py
+++ plot.py
@@ -18,14 +18,18 @@ EPSILON = 0.01
 NUM_BUCKETS = 500
 
 
-def make_bucket(proxy_vals, epsilon, x):
-    return np.abs(proxy_vals - x) < epsilon
+def make_bucket(bucket_vals, epsilon, x):
+    return np.abs(bucket_vals - x) < epsilon
 
-def proxy_mean(proxy_vals, bucket):
-    return np.mean(proxy_vals[bucket])
+def bucket_mean(bucket_vals, bucket):
+    return np.mean(bucket_vals[bucket])
 
-def real_mean(proxy_vals, real_vals, bucket):
-    return np.mean(real_vals[bucket])
+def sample_delta(samples, optimized_samples, bucket):
+    return np.mean(optimized_samples[bucket] - samples[bucket])
+
+def bucket_count(bucket):
+    count = np.sum(bucket)
+    return count if count != 0 else np.nan
 
 # def expand_bucket(bucket):
 #     first_true_i = np.argmax(bucket)
@@ -38,44 +42,62 @@ def real_mean(proxy_vals, real_vals, bucket):
 # def sign(x):
 #     return x / abs(x)
 
-# def bucket_count(xs, epsilon, x):
-#     return np.sum(abs(xs - x) < epsilon)
-
 def produce_traces(run):
     linear = np.linspace(-1, 1, NUM_BUCKETS)
 
     random_proxy = []
-    random_real = []
-
     optimized_proxy = []
+
+    random_real = []
     optimized_real = []
+
+    deltas = []
+
+    random_samples = []
+    optimized_samples = []
+    delta_samples = []
 
     for x in linear:
         random_bucket = make_bucket(run["random_proxy_vals"], EPSILON, x)
         optimized_bucket = make_bucket(run["optimized_proxy_vals"], EPSILON, x)
 
-        random_proxy.append(proxy_mean(run["random_proxy_vals"], random_bucket))
-        random_real.append(real_mean(run["random_proxy_vals"], run["random_real_vals"], random_bucket))
+        random_proxy.append(bucket_mean(run["random_proxy_vals"], random_bucket))
+        optimized_proxy.append(bucket_mean(run["optimized_proxy_vals"], optimized_bucket))
 
-        optimized_proxy.append(proxy_mean(run["optimized_proxy_vals"], optimized_bucket))
-        optimized_real.append(real_mean(run["optimized_proxy_vals"], run["optimized_real_vals"], optimized_bucket))
+        random_real.append(bucket_mean(run["random_real_vals"], random_bucket))
+        optimized_real.append(bucket_mean(run["optimized_real_vals"], optimized_bucket))
+
+        delta_bucket = make_bucket(run["optimized_proxy_vals"] - run["random_proxy_vals"], EPSILON, x)
+        deltas.append(bucket_mean(run["optimized_real_vals"] - run["random_real_vals"], delta_bucket))
+
+        # optimized_samples.append(bucket_count(optimized_bucket))
+        # random_samples.append(bucket_count(random_bucket))
+        # delta_samples.append(bucket_count(delta_bucket))
 
     return {
         "linear": linear,
         "random_proxy": random_proxy,
-        "random_real": random_real,
         "optimized_proxy": optimized_proxy,
+        "random_real": random_real,
         "optimized_real": optimized_real,
+        "random_samples": random_samples,
+        "optimized_samples": optimized_samples,
+        "delta_samples": delta_samples,
+        "deltas": deltas,
     }
+
+
+def get_traces_for_file(run_path):
+    print("Processing:", run_path)
+    with open(run_path, "rb") as f:
+        return pickle.load(f)
 
 
 if __name__ == "__main__":
     run_paths = glob.glob("runs/*.pickle")
     traces = collections.defaultdict(list)
     for run_path in run_paths:
-        print("Processing:", run_path)
-        with open(run_path, "rb") as f:
-            run_data = pickle.load(f)
+        run_data = get_traces_for_file(run_path)
         for trace_name, trace_array in produce_traces(run_data).items():
             traces[trace_name].append(trace_array)
 
@@ -89,9 +111,9 @@ if __name__ == "__main__":
     plt.plot(traces["linear"], traces["optimized_real"], label="Real Utility (Optimized)")
     plt.plot(traces["linear"], traces["optimized_real"] - traces["random_real"], label="Real Utility (Optimized - Random)")
 
-    # plt.plot(traces["linear"], traces["random_proxy"], label="Proxy Utility (Random)")
-    # plt.plot(traces["linear"], traces["optimized_proxy"], label="Proxy Utility (Optimized)")
     plt.plot(traces["linear"], traces["optimized_proxy"] - traces["random_proxy"], label="Proxy Utility (Optimized - Random)")
+
+    plt.plot(traces["linear"], traces["deltas"], label="Optimized - Random (Real vs. Proxy)")
 
     plt.legend()
     plt.xlabel("Proxy Utility")
